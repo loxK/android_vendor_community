307482a3eefd6bfb6ad1de0fd0902312ac60aed9
James Whitehead II <jnwhiteh@gmail.com>
2010-01-19 22:36:42 +0000

    Built AccountAndSyncSettings from source
    Since it's available, we should use this instead of a proprietary
    version from Google, so long as it works.

--- products/core.mk
+++ products/core.mk
@@ -20,6 +20,7 @@ $(call inherit-product, build/target/product/generic.mk)
 
 # This is the list of apps included in the build
 PRODUCT_PACKAGES := \
+	AccountAndSyncSettings \
 	AlarmClock \
 	AlarmProvider \
 	ApplicationsProvider \
